b8f6d517f3a453140222c81e73d5b9a66de35868
Raditfiransah <raditfiransah180@gmail.com>
2025-11-14 00:10:15 +0700

    000

---
+++ src/frontend/front.java
@@ -0,0 +1,87 @@
+import java.awt.*;
+import javax.swing.*;
+import net.miginfocom.swing.*;
+/*
+ * Created by JFormDesigner on Fri Nov 14 00:04:11 WIB 2025
+ */
+
+
+
+/**
+ * @author radit
+ */
+public class front extends JFrame {
+    public front() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        // Generated using JFormDesigner Evaluation license - radit firansah
+        dialogPane = new JPanel();
+        contentPanel = new JPanel();
+        spinner1 = new JSpinner();
+        buttonBar = new JPanel();
+        okButton = new JButton();
+
+        //======== this ========
+        var contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
+
+        //======== dialogPane ========
+        {
+            dialogPane.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new
+            javax. swing. border. EmptyBorder( 0, 0, 0, 0) , "JF\u006frmDes\u0069gner \u0045valua\u0074ion", javax
+            . swing. border. TitledBorder. CENTER, javax. swing. border. TitledBorder. BOTTOM, new java
+            .awt .Font ("D\u0069alog" ,java .awt .Font .BOLD ,12 ), java. awt
+            . Color. red) ,dialogPane. getBorder( )) ); dialogPane. addPropertyChangeListener (new java. beans.
+            PropertyChangeListener( ){ @Override public void propertyChange (java .beans .PropertyChangeEvent e) {if ("\u0062order" .
+            equals (e .getPropertyName () )) throw new RuntimeException( ); }} );
+            dialogPane.setLayout(new BorderLayout());
+
+            //======== contentPanel ========
+            {
+                contentPanel.setLayout(new MigLayout(
+                    "insets dialog,hidemode 3",
+                    // columns
+                    "[fill]" +
+                    "[fill]" +
+                    "[fill]",
+                    // rows
+                    "[]" +
+                    "[]" +
+                    "[]"));
+                contentPanel.add(spinner1, "cell 1 0");
+            }
+            dialogPane.add(contentPanel, BorderLayout.NORTH);
+
+            //======== buttonBar ========
+            {
+                buttonBar.setLayout(new MigLayout(
+                    "insets dialog,alignx right",
+                    // columns
+                    "[button,fill]",
+                    // rows
+                    null));
+
+                //---- okButton ----
+                okButton.setText("OK");
+                buttonBar.add(okButton, "cell 0 0");
+            }
+            dialogPane.add(buttonBar, BorderLayout.SOUTH);
+        }
+        contentPane.add(dialogPane, BorderLayout.CENTER);
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Evaluation license - radit firansah
+    private JPanel dialogPane;
+    private JPanel contentPanel;
+    private JSpinner spinner1;
+    private JPanel buttonBar;
+    private JButton okButton;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
